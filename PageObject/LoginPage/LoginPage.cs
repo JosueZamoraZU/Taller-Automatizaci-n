3967a0eb385b8bd88de1f524ab294b3530855838
Josue Zamora <josue.zamora@babelgroup.com>
2026-06-22 10:13:59 -0600

    Login Terminado

---
+++ PageObject/LoginPage/LoginPage.cs
@@ -0,0 +1,38 @@
+﻿using OpenQA.Selenium;
+using Practica_1.PageObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ecommers.PageObject.LoginPage
+{
+    public class LoginPage : BasePage
+    {
+        public LoginPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private By btnMyCuenta = By.XPath(("(//span[@class='title'][normalize-space()='My account'])[2]"));
+        private By btnLoginUser = By.XPath(("(//a[@class='list-group-item active'])[1]"));
+
+        private By tbxEmail = By.Id("input-email");
+        private By tbxContra = By.Id("input-password");
+
+        private By btnLogin = By.XPath("(//input[@value='Login'])[1]");
+
+
+        public void LoginUsuario(string email, string contra){
+
+            Click(btnMyCuenta);
+            Click(btnLoginUser);
+
+            IngresarInputs(tbxEmail, email);
+            IngresarInputs(tbxContra, contra);
+
+            Click(btnLogin);
+
+        }
+    }
+}
---
+++ Test/LoginTest/LoginTest.cs
@@ -0,0 +1,50 @@
+﻿using Ecommers.PageObject.LoginPage;
+using Ecommers.PageObject.RegistroPage;
+using OpenQA.Selenium.Support.UI;
+using Practica_1.Test;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ecommers.Test.LoginTest
+{
+    [TestFixture]
+    public class LoginTest : BaseTest
+    {
+        public LoginTest() : base("Login")
+        {
+        }
+
+        private LoginPage login;
+
+        [SetUp]
+
+        public void InicializarBrowser()
+        {
+            // Navegamos a la URL principal del ecommerce
+            Driver.Navigate().GoToUrl("https://ecommerce-playground.lambdatest.io/");
+
+            login = new LoginPage(Driver);
+        }
+
+        [Test]
+
+        public void Login() {
+
+            // Usas la variable que inicializaste en el Setup para ejecutar la acción
+            login.LoginUsuario("josue@email.com", "12345J");
+
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.UrlContains("route=account/account"));
+
+            // Tu assert para verificar que entraste a la cuenta
+            Assert.That(Driver.Url, Does.Contain("route=account"), "No se logró iniciar sesión.");
+
+            Thread.Sleep(1000);
+        }
+    }
+}
