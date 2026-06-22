90f4e3a4e901bb8a7fea7eca770f358d790d70fd
Josue Zamora <josue.zamora@babelgroup.com>
2026-06-22 17:49:49 -0600

    Flujo completo de compra / Buscar listo

---
+++ PageObject/BuscarPage/BuscarPage.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+using Practica_1.PageObject;
+
+namespace Ecommers.PageObject.BuscarPage
+{
+    public class BuscarPage : BasePage
+    {
+        public BuscarPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        private By tbxSearch = By.XPath("(//input[@placeholder='Search For Products'])[1]");
+
+        private By btnBuscar = By.XPath("(//button[normalize-space()='Search'])[1]");
+
+        public void BuscarProducto(string producto)
+        {
+            Click(tbxSearch);
+            IngresarInputs(tbxSearch, producto);
+
+            Click(btnBuscar);
+        }
+    }
+}
---
+++ Test/BuscarTest/BuscarTest.cs
@@ -0,0 +1,47 @@
+﻿using Ecommers.PageObject.BuscarPage;
+using OpenQA.Selenium.Support.UI;
+using Practica_1.Test;
+using SeleniumExtras.WaitHelpers;
+using System;
+using System.Threading;
+
+namespace Ecommers.Test.BuscarTest
+{
+    [TestFixture]
+    public class BuscarTest : BaseTest
+    {
+
+        private string producto1 = "iPhone";
+        private string producto2 = "iPod";
+        public BuscarTest() : base("Buscar")
+        {   
+        }
+
+        private BuscarPage search;
+
+        [SetUp]
+
+        public void InicializarBrowser()
+        {
+            // Navegamos a la URL principal del ecommerce
+            Driver.Navigate().GoToUrl("https://ecommerce-playground.lambdatest.io/");
+
+            search = new BuscarPage(Driver);
+        }
+
+        [Test]
+
+        public void Buscar() { 
+            
+            search.BuscarProducto(producto1);
+
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(10));
+            wait.Until(ExpectedConditions.UrlContains("route=product%2Fsearch&search=iPhone"));
+
+            // Verificar que entraste a la cuenta
+            Assert.That(Driver.Url, Does.Contain("route=product%2Fsearch&search=iPhone"), "No se logró iniciar sesión.");
+
+            Thread.Sleep(1000);
+        }
+    }
+}
